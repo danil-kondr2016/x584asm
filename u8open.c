3ac707361c0a39cf2f784a64e55ab93fefc39b07
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2025-03-03 10:38:37 +0600

    Add mode transcoding

--- u8open.c
+++ u8open.c
@@ -1,4 +1,5 @@
 #include "utils.h"
+#include "x584err.h"
 
 #include <stdlib.h>
 #ifdef _WIN32
@@ -12,17 +13,28 @@ FILE *u8open(char *u8path, char *mode)
 #ifdef _WIN32
 	{
 		wchar_t *wpath;
+		wchar_t *wmode;
 		int wpath_size;
+		int wmode_size;
 
 		wpath_size = MultiByteToWideChar(CP_UTF8, 0, u8path, -1, 0, 0) + 1;
 		wpath = calloc(wpath_size, sizeof(wchar_t));
 		if (!wpath)
-			return 0;
+			Die(X584ASM_FATAL_OUT_OF_MEMORY);
 		MultiByteToWideChar(CP_UTF8, 0, u8path, -1, wpath, wpath_size);
-		result = _wfopen(wpath, L"rb");
+
+		wmode_size = MultiByteToWideChar(CP_UTF8, 0, mode, -1, 0, 0) + 1;
+		wmode = calloc(wmode_size, sizeof(wchar_t));
+		if (!wmode)
+			Die(X584ASM_FATAL_OUT_OF_MEMORY);
+		MultiByteToWideChar(CP_UTF8, 0, mode, -1, wmode, wmode_size);
+
+		result = _wfopen(wpath, wmode);
+		free(wpath);
+		free(wmode);
 	}
 #else
-	result = fopen(u8path, "rb");
+	result = fopen(u8path, mode);
 #endif
 	return result;
 }
