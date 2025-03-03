0811a0f59735f56da0931ac6302ac93ad103808e
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2025-03-03 12:18:34 +0600

    Fix test1.c

--- test1.c
+++ test1.c
@@ -3,21 +3,23 @@
 #include "parser.h"
 #include "sds.h"
 #include "utf8proc.h"
+#include "program.h"
 
 int main(void)
 {
 	struct reader reader = {0};
 	struct lexer lexer = {0};
 	struct parser parser = {0};
+	struct program program = {0};
 	int32_t rune = 0;
 	char utf8[8] = {0};
 	sds token = NULL;
 
 	reader_open(&reader, "test.txt");
 	lexer_init(&lexer, &reader);
-	parser_init(&parser, &lexer);
+	parser_init(&parser, &lexer, &program);
 	parser_run(&parser);
-	parser_output(&parser, "output.x584");
+	program_output(&program, "output.x584");
 
 	return 0;
 }
