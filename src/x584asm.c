d6573a10c630e83de7dfe12d359bab8ff4e03a0d
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2025-05-11 06:45:43 +0000

    Set version to v0.2.13

--- src/x584asm.c
+++ src/x584asm.c
@@ -18,7 +18,7 @@
 
 #include <string.h>
 
-#define X584ASM_VERSION "v0.2.12"
+#define X584ASM_VERSION "v0.2.13"
 
 static const char str_compiled[] = __DATE__ " " __TIME__;
 static const char str_version[] = X584ASM_VERSION;
